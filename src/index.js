1e41074956d47a7c10e75992b8d87f8b807ee45a
VitaliyTom <tomashevich.vitaliy88@gmail.com>
2020-03-18 14:53:52 +0300

    feat: add solution morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,9 +38,50 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+    let string = '';
+
+    if (expr.includes('**********')) {
+        arr = expr.split('**********');
+    } else {
+        arr.push(expr);
+    }
+    
+    let arrayString = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        let array = [];
+        for (let j = 0; j < arr[i].length; j += 10) {
+            array.push(arr[i].slice(j, j + 10));
+        }
+        arrayString.push(array);
+    }
+
+    for (let i = 0; i < arrayString.length; i++) {
+        for (let j = 0; j < arrayString[i].length; j++) {
+            let wordStr = '';
+            for (let k = arrayString[i][j].length - 1; 0 < k; k -= 2) {
+                if (arrayString[i][j][k] === '0' && arrayString[i][j][k - 1] === '0') {
+                    continue;
+                } else if (
+                    arrayString[i][j][k] === '0' &&
+                    arrayString[i][j][k - 1] === '1'
+                ) {
+                    wordStr += '.';
+                } else if (
+                    arrayString[i][j][k] === '1' &&
+                    arrayString[i][j][k - 1] === '1'
+                ) {
+                    wordStr += '-';
+                }
+            }
+            string += MORSE_TABLE[wordStr.split('').reverse().join('')];
+        }
+        string += ' ';
+    }
+    return string.trim();
 }
 
 module.exports = {
     decode
-}
+};
